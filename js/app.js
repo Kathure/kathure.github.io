311ecd1b0d7dcc1976d1f6ba0f9bc15c794e652d
Kathure <lizkathure@gmail.com>
2016-06-18 12:33:05 +0300

    nimebadilisha landing page na portfolio

--- js/app.js
+++ js/app.js
@@ -19,3 +19,14 @@ $(document).ready(function () {
         $("#landing").removeClass("is-loading");
     }, 100)
 });
+
+$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() - 70;
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+});
