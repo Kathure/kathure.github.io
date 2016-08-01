673b24697c0eb38407254e720ffc69ac70d4b6bc
Kathure <lizkathure@gmail.com>
2016-08-01 10:33:41 +0300

    changed links due to updates

--- js/app.js
+++ js/app.js
@@ -38,3 +38,17 @@ $('document').ready(function () {
     var msg = $('#message');
     msg.autosize();
 });
+
+$(function () {
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
+    $('.nav .toggle-nav').click(function (e) {
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
